f6aa67fdca783a0b9e0f209b162552f83ff46e8f
Hao Fu <markfu1022@hotmail.com>
2018-05-11 21:30:43 -0700

    added db charset

--- connector_v2.py
+++ connector_v2.py
@@ -6,7 +6,7 @@ import pymysql
 str_url_master = "https://losangeles.craigslist.org/d/housing/search/hhh"
 num_page_range = 10
 
-conn = create_engine('mysql+pymysql://LARentalScraper:LAscraper2018@scraperdb.cdfom3jx46ff.us-west-1.rds.amazonaws.com:3306/ScraperDB')
+conn = create_engine('mysql+pymysql://LARentalScraper:LAscraper2018@scraperdb.cdfom3jx46ff.us-west-1.rds.amazonaws.com:3306/ScraperDB?charset=utf8')
 
 df = scraper.scraper(str_url_master, num_page_range)
 
