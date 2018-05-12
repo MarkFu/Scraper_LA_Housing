564250aec13cd1cf781da3a18786ec3e934c3e82
Hao Fu <markfu1022@hotmail.com>
2018-05-11 21:29:48 -0700

    Updated connector_v2

--- connector_v2.py
+++ connector_v2.py
@@ -1,12 +1,13 @@
 import pandas as pd
 from sqlalchemy import create_engine
 import scraper
+import pymysql
 
 str_url_master = "https://losangeles.craigslist.org/d/housing/search/hhh"
 num_page_range = 10
 
-conn = create_engine('mysql+mysqldb://LARentalScraper:LAscraper2018@scraperdb.cdfom3jx46ff.us-west-1.rds.amazonaws.com:3306/ScraperDB')
+conn = create_engine('mysql+pymysql://LARentalScraper:LAscraper2018@scraperdb.cdfom3jx46ff.us-west-1.rds.amazonaws.com:3306/ScraperDB')
 
 df = scraper.scraper(str_url_master, num_page_range)
 
-df.io.sql.to_sql('tbl_LA_Rental_Listing', con = conn, if_exists = 'append', index = False)
+df.to_sql('tbl_LA_Rental_Listing', con = conn, if_exists = 'append', index = False)
